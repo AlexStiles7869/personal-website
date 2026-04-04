47b86c73f144bb7f123f3e1ec1d444378c2ecaaa
Alex Stiles <ast195@uclive.ac.nz>
2026-04-04 16:39:37 +1100

    add image optimisation

--- static_version/eleventy.config.js
+++ static_version/eleventy.config.js
@@ -3,7 +3,20 @@ import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
 import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 
 export default async function(eleventyConfig) {
-	eleventyConfig.addPlugin(eleventyImageTransformPlugin);
+	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+		// which formats to generate
+		formats: ["avif", "webp", "jpeg"],
+		
+		// widths to generate for all images
+		widths: [400, 800, 1200, "auto"],
+
+		// optional: attributes to add to the generated <picture> or <img>
+		defaultAttributes: {
+			loading: "lazy",
+			decoding: "async",
+		},
+	});
+
 	eleventyConfig.addPlugin(eleventyNavigationPlugin);
 	eleventyConfig.addPlugin(syntaxHighlight);
 
@@ -12,7 +25,8 @@ export default async function(eleventyConfig) {
 	return {
 		dir: {
 			input: "src",
-			output: "_site"
+			output: "_site",
+			includes: "_includes"
 		}
 	};
 };
